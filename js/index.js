2a2fda93097be29aed9b8214e4c2028bb230afc4
Patryk Bialek <p.bialek@kaft.pl>
2021-02-28 12:03:45 +0100

    build html content

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const actionContainer = document.querySelector("#actionContainer");
+const logoutButton = document.querySelector("#logoutButton");
+const actionSpinner = document.querySelector("#actionSpinner");
+const actionMessage = document.querySelector("#actionMessage");
+
+const message = {
+  logoutBefore: "Możesz się bezpiecznie wylogować.",
+  logounInProgress: "Trwa bezpiecznie wylogowywanie.",
+};
+
+const logoutButtonContent = {
+  logout: "Wyloguj",
+  spinner: "<div class='tm-action__spinner'></div>",
+};
+
+logoutButton.addEventListener("click", logout);
+
+function logout() {
+  logoutButton.innerHTML = logoutButtonContent.spinner;
+  actionMessage.innerHTML = message.logounInProgress;
+
+  logoutCall();
+}
+
+function logoutCall() {
+  setTimeout(() => {
+    logoutButton.innerHTML = logoutButtonContent.logout;
+    actionMessage.innerHTML = message.logoutBefore;
+  }, 3000);
+}
